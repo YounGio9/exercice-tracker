e67f590b4789082ff635e2061b0bc826c3cf7c76
Giovani <desouzagiovani9@gmail.com>
2023-03-08 00:39:54 +0100

    Logs exercices works perfectly

--- index.js
+++ index.js
@@ -6,12 +6,16 @@ const bodyParser = require('body-parser')
 require('dotenv').config()
 
 app.use(cors())
-app.use(bodyParser())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 app.use(express.static('public'))
 
 const connect = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI,  { useNewUrlParser: true, useUnifiedTopology: true })
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
     console.log('connected to MongoDB')
   } catch (error) {
     console.error(error)
@@ -21,146 +25,144 @@ connect()
 
 const exerciseSchema = mongoose.Schema({
   username: String,
-  user_id: {type: String, required: true},
-  description: {type: String, required: true},
-  duration: {type: Number, required: true},
+  user_id: { type: String, required: true },
+  description: { type: String, required: true },
+  duration: { type: Number, required: true },
   date: {
     type: Date,
-    required: true
+    required: true,
   },
 })
 
 const Exercise = mongoose.model('Exercise', exerciseSchema)
 
 const userSchema = mongoose.Schema({
-  username: {type: String, required: true},
+  username: { type: String, required: true },
 })
 
 const User = mongoose.model('User', userSchema)
 
 const logSchema = mongoose.Schema({
-  username: {type: String, required: true},
-  user_id: {type: String, required: true},
-  count: {type: Number, required: true},
-  log: [{
-    description: {type: String, required: true},
-    duration: {type: Number, required: true},
-    date: {type: Date, required: true}
-  }]
+  username: { type: String, required: true },
+  user_id: { type: String, required: true },
+  count: { type: Number, required: true },
+  log: [
+    {
+      description: { type: String, required: true },
+      duration: { type: Number, required: true },
+      date: { type: Date, required: true },
+    },
+  ],
 })
 
 const Log = mongoose.model('Log', logSchema)
 
-
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
-});
+})
 
-app.get('/api/users/:id/logs', async (req, res) => {
-  const {id} = req.params
+app.post('/api/users', async (req, res) => {
+  const { username } = req.body
   try {
-    const userLog = await Log.findOne({user_id: id})
-    const {username, count} = userLog
-    res.status(200).json({
-      _id: userLog.user_id,
-      username,
-      count,
-      log: userLog.log.map(log => {
-        return {
-          description: log.description,
-          duration: log.duration,
-          date: log.date
-        } 
-      })
+    const user = await User.create({ username: username })
+    user.save()
+    const log = await Log.create({
+      username: user.username,
+      user_id: user._id,
+      count: 0,
+      log: [],
+    })
 
+    log.save()
+
+    res.status(201).json({
+      username: user.username,
+      _id: user._id,
     })
   } catch (error) {
-    console.error(error.message)
+    return console.error(error.message)
   }
 })
 
-app.post('/api/users', async (req,res) => {
+app.get('/api/users/:_id/logs', async (req, res) => {
+  const { _id } = req.params
 
-  const {username} = req.body
-    try {
-      const user = await User.create({username: username})
-      user.save()
-      res.status(201).json({
-        username: user.username,
-        _id: user._id
-      })
-    } catch (error) {
-      return console.error(error.message)
-    }
+  Log.findOne({ user_id: _id }, (err, userLog) => {
+    if (err) return res.send('error')
+    if (!userLog) return res.send("user doesn't exists")
+
+    const { username, count } = userLog
+
+    res.json({
+      _id: userLog.user_id,
+      username,
+      count,
+      log: userLog.log.map((log) => {
+        return {
+          description: log.description,
+          duration: log.duration,
+          date: log.date.toDateString(),
+        }
+      }),
+    })
+    console.log(req.body, req.params, req.query)
+  })
 })
 
 app.get('/api/users', async (req, res) => {
-  let users 
+  let users
   try {
-     users = await User.find()
+    users = await User.find()
   } catch (error) {
     return console.error(error.message)
   }
   res.status(200).json(users)
 })
 
-app.post('/api/users/:_id/exercises',async (req,res) => {
-  const {id} = req.params
-  const {_id, description, duration, date} = req.body
-  const d = date === '' ? Date.now() : new Date(date)
+app.post('/api/users/:_id/exercises', async (req, res) => {
+  const { _id } = req.params
+  const { description, duration } = req.body
+  let d
+
+  if (req.body.date) {
+    const { date } = req.body
+    d = new Date(date)
+
+    if (d == 'Invalid Date' || null) return res.json({ err: 'Invalid date' })
+  } else {
+    d = Date.now()
+  }
 
   try {
-    const user = await User.findById({_id: _id}).select({__v: 0})
-     const exercise = await Exercise.create({
+    const user = await User.findById({ _id: _id }).select({ __v: 0 })
+    const exercise = await Exercise.create({
       username: user.username,
-      description: description, 
+      description: description,
       duration: duration,
       user_id: _id,
-      date: d
+      date: d,
     })
     exercise.save()
-    const log = await Log.findOne({user_id: _id})
-    if(log) {
-      log.count = log.count + 1
-      log.log.push({
-        description: description,
-        date: d,
-        duration: duration
-      })
-      log.save()
-    } 
-    else {
-      await Log.create({
-        username: user.username,
-        user_id: _id,
-        count: 1,
-        log: [
-          {
-            description: description,
-            duration: duration,
-            date: d
-          }
-        ]
-      })
-      console.log('Log not exist then created')
-    }
+    const log = await Log.findOne({ user_id: _id })
+
+    log.count = log.count + 1
+    log.log.push({
+      description: description,
+      date: d,
+      duration: duration,
+    })
+    log.save()
+
     res.status(201).send({
       ...user._doc,
       date: exercise.date.toDateString(),
       duration: exercise.duration,
-      description: exercise.description
+      description: exercise.description,
     })
   } catch (error) {
     console.error(error.message)
   }
-  
 })
-
-
-
-
-
-
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
